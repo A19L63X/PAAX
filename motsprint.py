176600df3275bcfdd1abd2989308e9c7cc0f9a14
A19L63X <antarcticax63@gmail.com>
2025-05-03 19:44:34 +0200

    Mensaje descriptivo de los cambios

--- motsprint.py
+++ motsprint.py
@@ -10,7 +10,8 @@ headers = {
 }
 
 urls = [
-    "https://es.motorsport.com/f1/results/2025/gp-de-china-653027/?st=SPR"
+    "https://es.motorsport.com/f1/results/2025/gp-de-china-653027/?st=SPR",
+https://es.motorsport.com/f1/results/2025/gp-de-miami-653353/?st=SPR
 ]
 
 output_base_dir = os.path.dirname(os.path.abspath(__file__))  # Ruta del script
