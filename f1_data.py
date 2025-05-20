b76cf0b2c8bffed1bf51d58184bbb5e004525fd3
A19L63X <antarcticax63@gmail.com>
2025-05-20 18:38:29 +0200

    arg

--- f1_data.py
+++ f1_data.py
@@ -108,6 +108,13 @@ def generate_html(data):
 
         # Agregar filas para cada piloto de la carrera
         for driver in season_data['results']:
+
+            # Añadir bandera argentina para Franco Colapinto
+            driver_display = driver['driver']
+            if "Franco Colapinto" in driver['driver']:
+                driver_display = f"<img src='images/flags/flag_of_argentina.svg' class='flag' alt='Argentina'>{driver['driver']}"
+
+
             html_content += f"""
             <tr>
                 <td>{driver['position']}</td>
