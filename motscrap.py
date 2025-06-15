74e8625b5b673baff466544305ede7afb699778c
A19L63X <antarcticax63@gmail.com>
2025-06-15 18:58:03 +0200

    Update motscrap.py

--- motscrap.py
+++ motscrap.py
@@ -17,7 +17,7 @@ urls = [
     "https://es.motorsport.com/f1/results/2025/gp-de-miami-653353/?st=GRID",
     "https://es.motorsport.com/f1/results/2025/gp-emilia-romagna-653356/?st=GRID",
     "https://es.motorsport.com/f1/results/2025/gp-de-monaco-653359/?st=GRID",
-    "https://es.motorsport.com/f1/results/2025/gp-de-espana-653362/?st=GRID"
+    "https://es.motorsport.com/f1/results/2025/gp-de-espana-653362/?st=GRID",
     "https://es.motorsport.com/f1/results/2025/gp-de-canada-653365/?st=GRID"
 ]
 
